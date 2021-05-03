1845ffa3a4e3ccc79423ab4cb56d65d3ecea9cfe
Abby Benjamin <ablizben@gmail.com>
2021-05-02 22:14:34 -0400

    editing about section

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -26,7 +26,7 @@ const Contact = () => {
             </div>
             </div>
         </div>
-            <p id="not-dark" className="Copy">2020 © Copyright <strong>{contact.copyright}</strong>. All Rights Reserved</p>
+            <p id="not-dark" className="Copy">2021 © Copyright <strong>{contact.copyright}</strong>. All Rights Reserved</p>
         </div>
     )
     
