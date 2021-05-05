93942436e82e3586b1397f0bddee5fd55dbc8133
Abby Benjamin <ablizben@gmail.com>
2021-05-05 17:33:54 -0400

    redoing color scheme of portfolio

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { social } from '../Content';
+// import { social } from '../Content';
 
 
 
@@ -17,7 +17,8 @@ const Header = () => {
         <div>
             <div className="Header">
                 <h1>abby benjamin</h1>
-                <div className="header-icons">
+                <h2>frontend developer</h2>
+                {/* <div className="header-icons">
                      <div className="social-icons">
                             {social.linkedin && <a title="Visit Linkedin profile" rel="noopener noreferrer" target="_blank"  href={social.linkedin}><i className="fab fa-linkedin fa-3x" id="linkedin"></i></a>}
                             {social.github && <a title="Visit Github profile" rel="noopener noreferrer" target="_blank" href={social.github}><i className="fab fa-github fa-3x" id="github"></i></a>}<br/>
@@ -25,7 +26,8 @@ const Header = () => {
                     <div className="resume-icon">
                             {social.resume && <a title="Download Resume" href={social.resume} download><i className="fas fa-download fa-3x" id="resume"></i></a>}
                     </div>
-                </div>
+                </div> */}
+                <span></span>
             </div>
         </div>
        
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Favicon from '../img/favicon-32x32.png';
+// import Favicon from '../img/favicon-32x32.png';
 
 
 const NavBar = () => {
 
 return (
     <div className="NavBar">
-            <img src={Favicon} id="favicon" alt="website logo"/>
+            {/* <img src={Favicon} id="favicon" alt="website logo"/> */}
+            <div id="favicon">FAVICON</div>
   
         <div className="nav-links">
             <a href=".NavBar">Home</a>
